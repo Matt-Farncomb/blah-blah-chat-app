552ffe6ac137342a6c0e4c1490a8e3e62d9eb388
Matt <mfarncomb101@gmail.com>
2019-02-13 22:45:10 +0200

    testing

--- static/channel.js
+++ static/channel.js
@@ -100,7 +100,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     // Connect to websocket
-    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port, {transports: ['websocket']});
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
     // Text grabbed from chat msg window and sent to server
     function sendMessage() {
